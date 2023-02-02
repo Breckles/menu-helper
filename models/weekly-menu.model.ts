0ce66fae22eca10e1698474a65e2e4c07f6692b0
Waylon <wayloncn@gmail.com>
2023-02-02 13:18:27 -0500

    Makes a change to weekly-menu model

--- models/weekly-menu.model.ts
+++ models/weekly-menu.model.ts
@@ -1,7 +1,9 @@
+import IDailyMenu from './daily-menu.model';
+
 interface IWeeklyMenu {
   id: string;
   weekStartDate: string;
-  dishes: any[];
+  dishes: IDailyMenu[];
 }
 
 export default IWeeklyMenu;
